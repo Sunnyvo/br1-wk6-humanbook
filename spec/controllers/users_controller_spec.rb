8a92ca0f9a5e5c7387c4b3435d573ceae5cec9df
Sunny <votanphu.2101@gmail.com>
2017-08-15 15:50:46 +0700

    move circle.yml 1

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -9,7 +9,6 @@ RSpec.describe UsersController, type: :controller do
       it "returns a success response" do
         get :index, params: {}, session: valid_session
         expect(response).to be_success
-        
       end
     end
 
